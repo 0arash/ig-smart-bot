91fd814d2d06a0de9eb2dc62bb6988876333d31e
0arash <arashgh.dev@gmail.com>
2024-02-04 16:19:34 +0330

    added fafa scraper

--- src/utils/scrapers/fafait.ts
+++ src/utils/scrapers/fafait.ts
@@ -3,50 +3,53 @@ import parseString from "xml2js";
 import fs from "node:fs/promises";
 
 async function getFafaItProducts(baseUrl: string) {
-  try {
-    const res = await axios.get(baseUrl);
-    console.log(`code: ${res.status}`);
-    
-    const parser = await parseString.parseStringPromise(res.data);
-    
-    var urlList: { loc: string[] }[] = parser["urlset"]["url"];
-    console.log("products count:", urlList.length);
-    const path = `./src/products/${
-      baseUrl.replace("https://", "").split("/")[0]
-    }`;
-    const urls: string[] = [];
-    // for (var i = 0; i < urlList.length; i++) {
-    for (var i = 0; i < 3; i++) {
-      const url = urlList[i].loc;
-      const product = await axios.get(url[0]);
-      const strHtml: string = product.data;
+    try {
+        const res = await axios.get(baseUrl);
+        console.log(`code: ${res.status}`);
 
-      const regexTitle: RegExp = /<h1 class="product-name">(.*?)<\/h1>/;
-      const regexSpec: RegExp = /<ul class="technical-specs">(.*?)<\/ul>/;
-      const regexPrice: RegExp =
-        /<span class="h1 mb-0" data-v-b09b87e2="">(.*?)<small data-v-b09b87e2=""><span class="small text-muted mr-1" data-v-b09b87e2="">تومان<\/span><\/small><\/span>/;
+        const parser = await parseString.parseStringPromise(res.data);
 
-      const matchTitle: RegExpExecArray | null = regexTitle.exec(strHtml);
-      const matchPrice: RegExpExecArray | null = regexPrice.exec(strHtml);
-      const matchSpec: RegExpExecArray | null = regexSpec.exec(strHtml);
+        var urlList: { loc: string[] }[] = parser["urlset"]["url"];
+        console.log("products count:", urlList.length);
+        const path = `./src/products/${
+            baseUrl.replace("https://", "").split("/")[0]
+        }`;
+        let results: object[] = [];
+        // for (var i = 0; i < urlList.length; i++) {
+            for (var i = 0; i < 50; i++) {
+            const url = urlList[i].loc;
+            const product = await axios.get(url[0]);
+            const strHtml: string = product.data;
 
-      const title: string | null = matchTitle ? matchTitle[1] : null;
-      const price: string | null = matchPrice ? matchPrice[1] : null;
-      const spec: string | null = matchSpec ? matchSpec[1] : null;
+            const regexTitle: RegExp = /<h1 class="product-name">(.*?)<\/h1>/;
+            const regexSpec: RegExp = /<ul class="technical-specs">(.*?)<\/ul>/;
+            const regexPrice: RegExp =
+                /<span class="h1 mb-0" data-v-b09b87e2="">(.*?)<small data-v-b09b87e2=""><span class="small text-muted mr-1" data-v-b09b87e2="">تومان<\/span><\/small><\/span>/;
 
-      console.log(title);
-      console.log(price);
-      console.log(spec);
+            const matchTitle: RegExpExecArray | null = regexTitle.exec(strHtml);
+            const matchPrice: RegExpExecArray | null = regexPrice.exec(strHtml);
+            const matchSpec: RegExpExecArray | null = regexSpec.exec(strHtml);
+
+            const title: string | null = matchTitle ? matchTitle[1] : null;
+            const price: string | null = matchPrice ? matchPrice[1] : null;
+            const spec: string | null = matchSpec ? matchSpec[1] : null;
+
+            results.push({
+                title,
+                price,
+                spec,
+            });
+        }
+        await fs.writeFile("fafait.json", JSON.stringify(results));
+        // const urlsJson = {
+        //   urls: urls,
+        // };
+
+        // await fs.writeFile(`${path}.json`, JSON.stringify(urlsJson));
+        console.log("Done");
+    } catch (error) {
+        console.log(error);
     }
-    // const urlsJson = {
-    //   urls: urls,
-    // };
-
-    // await fs.writeFile(`${path}.json`, JSON.stringify(urlsJson));
-    console.log("Done");
-  } catch (error) {
-    console.log(error);
-  }
 }
 
 console.clear();
